b99ea54471020523b75cc2ec3b171c81401bc8db
Nilhcem <nilhcem@gmail.com>
2014-05-08 17:28:04 +0200

    Add MemoryClient.play method

--- xke-memory-client/xke-memory-client/src/main/java/com/nilhcem/xebia/memory/client/MemoryClient.java
+++ xke-memory-client/xke-memory-client/src/main/java/com/nilhcem/xebia/memory/client/MemoryClient.java
@@ -1,5 +1,7 @@
 package com.nilhcem.xebia.memory.client;
 
+import com.nilhcem.xebia.memory.client.model.Card;
+import com.nilhcem.xebia.memory.client.model.PlayResponse;
 import com.nilhcem.xebia.memory.client.rest.RetrofitService;
 import com.nilhcem.xebia.memory.client.rest.api.MemoryApi;
 import org.slf4j.Logger;
@@ -12,15 +14,22 @@ public class MemoryClient {
     private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
     private MemoryApi api;
+    private Grid grid;
 
     @Inject
-    public MemoryClient(RetrofitService retrofitService) {
-        api = retrofitService.get(MemoryApi.class);
+    public MemoryClient(RetrofitService retrofitService, Grid grid) {
+        this.api = retrofitService.get(MemoryApi.class);
+        this.grid = grid;
     }
 
     public void start() {
         LOG.debug("Starting Memory Client");
 
         LOG.debug("Finished");
+    }
+
+    PlayResponse play(Card[] toPlay) {
+        LOG.debug("Playing cards: {}", toPlay);
+        return api.play(new int[][]{new int[]{toPlay[0].getX(), toPlay[0].getY()}, new int[]{toPlay[1].getX(), toPlay[1].getY()}});
     }
 }
---
+++ xke-memory-client/xke-memory-client/src/test/java/com/nilhcem/xebia/memory/client/MemoryClientTest.java
@@ -0,0 +1,60 @@
+package com.nilhcem.xebia.memory.client;
+
+import com.nilhcem.xebia.memory.client.model.Card;
+import com.nilhcem.xebia.memory.client.model.PlayResponse;
+import com.nilhcem.xebia.memory.client.rest.RetrofitService;
+import com.nilhcem.xebia.memory.client.rest.api.MemoryApi;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import retrofit.http.Body;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class MemoryClientTest {
+
+    private MemoryClient client;
+    private TestMemoryApi api = new TestMemoryApi();
+
+    @Mock
+    private RetrofitService service;
+
+    @Mock
+    private Grid grid;
+
+    @Before
+    public void setup() {
+        initMocks(this);
+        when(service.get(MemoryApi.class)).thenReturn(api);
+        client = new MemoryClient(service, grid);
+    }
+
+    @Test
+    public void should_call_api_with_coords_from_cards() {
+        // Given
+        Card card1 = new Card(2, 4);
+        Card card2 = new Card(3, 1);
+
+        // When
+        client.play(new Card[]{card1, card2});
+
+        // Then
+        assertThat(api.cardsPositions[0][0]).isEqualTo(2);
+        assertThat(api.cardsPositions[0][1]).isEqualTo(4);
+        assertThat(api.cardsPositions[1][0]).isEqualTo(3);
+        assertThat(api.cardsPositions[1][1]).isEqualTo(1);
+    }
+
+    private static class TestMemoryApi implements MemoryApi {
+
+        int[][] cardsPositions;
+
+        @Override
+        public PlayResponse play(@Body int[][] cardsPositions) {
+            this.cardsPositions = cardsPositions;
+            return new PlayResponse();
+        }
+    }
+}
